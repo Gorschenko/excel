028eb6d3983d928ec04e2f6505fb64505f10c831
Gorschenko <aa.gorschenko@yandex.ru>
2022-07-09 17:25:55 +0300

    Dom listeners were edited

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,3 +1,5 @@
+import { capitalize } from '@/core/utils'
+
 export class DomListener {
   costructor($root, listeners = []) {
     if (!$root) {
@@ -7,9 +9,19 @@ export class DomListener {
     this.listeners = listeners
   }
   initDOMListeners() {
-
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener)
+      if (!this[method]) {
+        throw new Error()
+      }
+      this.$root.on(listener, this[method].bind(this))
+    })
   }
   removeDOMListeners() {
 
   }
+}
+
+function getMethodName(eventName) {
+  return 'on' + capitalize(eventName)
 }
