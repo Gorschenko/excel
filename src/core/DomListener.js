c413e9d4d2ed24277ef8994efe9d4b0f716cbf17
Gorschenko <aa.gorschenko@yandex.ru>
2022-07-09 16:41:58 +0300

    Listeners have added for some components

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,3 +1,15 @@
 export class DomListener {
+  costructor($root, listeners = []) {
+    if (!$root) {
+      throw new Error('No $rppt provided for DomListener')
+    }
+    this.$root = $root
+    this.listeners = listeners
+  }
+  initDOMListeners() {
 
+  }
+  removeDOMListeners() {
+
+  }
 }
